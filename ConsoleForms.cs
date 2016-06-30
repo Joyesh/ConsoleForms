2db8efe1f6703f72691316bd5f68c64b162e3d4e
Joyesh <Stoncel@inbox.com>
2016-06-30 10:13:53 -0500

    Adding updated development ConsoleForms files.

---
+++ ConsoleForms.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForms
+{
+    public class FormWindow 
+    {
+        // They created a new form instance, let's initalize everything.
+        public List<ConsoleControl> objects;
+        public bool Visible = false;
+        public bool CloseButton = true;
+        //public bool MinimizeButton; Not implemented yet.
+        //public bool MaximizeButton; Not implemented yet.
+        public string Title = "NewConsoleForm";
+        public int Width = 18; //In characters, not counting window edges.
+        public int Height = 7; //In characters, not counting window edges.
+        public void Show()
+        {
+            //This code regenerates the whole form.
+
+        }
+    }
+
+    public class ConsoleControl
+    {
+        //Main class for every control.
+        public bool Visible = true;
+    }
+    public class ConsoleTextBox : ConsoleControl
+    {
+
+    }
+    public class ConsoleLabel : ConsoleControl
+    {
+
+    }
+}
